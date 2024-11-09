6124a3740d8221a919c1646697348d571b2a968b
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2024-11-08 22:08:23 -0500

    array instead of pandas frame?

--- scripts/utils.py
+++ scripts/utils.py
@@ -1431,8 +1431,9 @@ def load_data(updated_koi_params=True, only_candidates=True):
     assert np.unique(dftoi['disposition']).size == 3
 
     # make these useful degrees like all the other catalogs
-    dftoi['ra'] = Angle(dftoi['RA'], unit='hourangle').degree
-    dftoi['dec'] = Angle(dftoi['Dec'], unit='degree').degree
+    breakpoint()
+    dftoi['ra'] = Angle(dftoi['RA'].values, unit='hourangle').degree
+    dftoi['dec'] = Angle(dftoi['Dec'].values, unit='degree').degree
 
     # assume symmetrical errors as they seem to do
     dftoi['rade_err2'] = -1 * dftoi['rade_err1']
