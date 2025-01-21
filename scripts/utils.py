f4d445cc230364b8ca9df87480f353b993d81f53
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2025-01-20 22:42:10 -0500

    lots of BDs added

--- scripts/utils.py
+++ scripts/utils.py
@@ -1460,7 +1460,9 @@ def load_data(updated_koi_params=True, only_candidates=True):
            'TOI-569.01', 'TOI-629.01', 'TOI-1406.01', 'TOI-1417.01',
            'TOI-2119.01', 'TOI-1278.01', 'TOI-2543.01', 'TOI-5081.01',
            'TOI-5090.01', 'TOI-4571.01', 'TOI-6970.01', 'TOI-4454.01',
-           'TOI-2336.01']
+           'TOI-2336.01', 'TOI-2844.01', 'TOI-3577.01', 'TOI-3755.01',
+           'TOI-4462.01', 'TOI-4737.01', 'TOI-4759.01', 'TOI-5240.01',
+           'TOI-5467.01', 'TOI-5882.01']
     for ibd in bds:
         bd = np.where(dftoi['name'] == ibd)[0][0]
         assert dftoi.loc[bd, 'disposition'] == 'Confirmed'
